068fdc4fd51a25662aafc9946cd4e55bb86a133e
Jovita Mariam V <130071853+JovitaMariam@users.noreply.github.com>
2025-08-10 19:38:22 +0530

    Update hellojovita_prl.h

--- 25MAI0078/hellojovita_prl.h
+++ 25MAI0078/hellojovita_prl.h
@@ -1,8 +1,11 @@
-#include "stdio.h"
+#include <stdio.h>
 #include "omp.h"
-
-void fn_hellojovita_prl() {
-
+void fn_hellojovita_prl()
+{
 #pragma omp parallel
-	printf("Hello Jovita Parallel\n");
+	{
+		printf("Hello World from thread %d\n", omp_get_thread_num());
+	}
 }
+
+
